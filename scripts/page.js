b3f7b77e82466b9bc9334c375d5a4edf49b12be0
efox12 <erikfox@Eriks-MacBook-Pro-3.local>
2018-10-28 14:49:05 -0700

    more debug

--- scripts/page.js
+++ scripts/page.js
@@ -346,7 +346,7 @@ window.addEventListener('touchstart', function(e){
         startY = touchobj.pageY;
         startTime = new Date().getTime() // record time when finger first makes contact with surface
         //e.preventDefault()
-        console.log(startX)
+        console.log("start"+startX)
 
     }, false)
  
@@ -359,7 +359,7 @@ window.addEventListener('touchend', function(e){
         dist = touchobj.pageX - startX; // get total dist traveled by finger while in contact with surface
         dist2 = startX - touchobj.pageX;
         elapsedTime = new Date().getTime() - startTime // get time elapsed
-        console.log(touchobj.pageX)
+        console.log("end"+touchobj.pageX)
         // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
         var swipeRightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
         var swipeLeftBol = (elapsedTime <= allowedTime && dist2 >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
