38becb12d7ab653a69f857dea4eb430ad789c6a4
efox12 <erikfox@Eriks-MacBook-Pro-3.local>
2018-10-27 19:25:09 -0700

    Basic Changes

--- main.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-//var dt = require('./myfirstmodule');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
