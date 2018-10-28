8b1e1b17551550481aa9849b24779510ca487a4c
efox12 <erikfox@Eriks-MacBook-Pro-3.local>
2018-10-28 14:00:46 -0700

    Swipe

--- scripts/page.js
+++ scripts/page.js
@@ -307,3 +307,55 @@ function updateYPos(){
         drawer3.style.backgroundColor = "white";
     }
 }
+
+var touchsurface = document.getElementById('touchsurface'),
+        startX,
+        startY,
+        dist,
+        threshold = 25, //required min distance traveled to be considered swipe
+        allowedTime = 200, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime
+ 
+    function handleswipe(isrightswipe){
+        if (isrightswipe){
+            clicked = true;
+            drawer.style.width = "40%";
+
+            drawer0.style.opacity = "1";
+            drawer1.style.opacity = "1";
+            drawer2.style.opacity = "1";
+            drawer3.style.opacity = "1";
+        } else{
+            drawer.style.width = "0%";
+
+            drawer0.style.opacity = "0";
+            drawer1.style.opacity = "0";
+            drawer2.style.opacity = "0";
+            drawer3.style.opacity = "0";
+        }
+    }
+ 
+    touchsurface.addEventListener('touchstart', function(e){
+        touchsurface.innerHTML = ''
+        var touchobj = e.changedTouches[0]
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        e.preventDefault()
+    }, false)
+ 
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault() // prevent scrolling when inside DIV
+    }, false)
+ 
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+        var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+        handleswipe(swiperightBol)
+        e.preventDefault()
+  }, false)
